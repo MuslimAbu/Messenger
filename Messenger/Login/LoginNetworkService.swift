927de99ec4aff2e748aef108a7298a967d4f4ea8
M <M>
2024-05-14 21:09:42 +0300

    L-46

---
+++ Messenger/Login/LoginNetworkService.swift
@@ -0,0 +1,8 @@
+//
+//  LoginNetworkService.swift
+//  Messenger
+//
+//  Created by Муслим on 14.05.2024.
+//
+
+import Foundation
---
+++ Messenger/Register/RegisterNetworkService.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterNetworkService.swift
+//  Messenger
+//
+//  Created by Муслим on 11.05.2024.
+//
+
+import Foundation
